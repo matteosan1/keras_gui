6d3eaed6282e990caa8a79d5a7b16131bda2e9bb
Matteo Sani <matteo.sani@mpscs.it>
2023-07-13 17:27:13 +0200

    [UPD] Add editable table prototype

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import pickle, numpy as np
 
+from keras.models import Sequential
+from keras.layers import Dense
+
 from os import urandom
 from os.path import join
+
 from flask import Flask, request, render_template, url_for, flash, redirect
 from werkzeug.utils import secure_filename
-#from keras.models import load_model
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = urandom(24).hex()
@@ -17,6 +21,59 @@ messages = [{'title': 'Message One',
             {'title': 'Message Two',
              'content': 'Message Two Content'}
             ]
+import threading
+import time
+from tqdm import tqdm
+
+# replace with train model - suppose it takes 3 seconds to train
+def _trainModel():
+  time.sleep(3)
+  return
+# replace with load data function - suppose it takes 2 seconds to load
+def _loadData():
+  time.sleep(2)
+  return
+
+# assume we loop 10 times
+epochs = 10
+
+class TrainModel (threading.Thread):
+    def __init__(self, data):
+        threading.Thread.__init__(self)
+        self.data = data
+    def run(self):
+        # return model loss
+        self._return = _trainModel()    
+    def join(self):
+        threading.Thread.join(self)
+        return self._return
+    
+class LoadData (threading.Thread):
+    def __init__(self, filenames):
+        threading.Thread.__init__(self)
+        self.filenames = filenames
+    def run(self):        
+        # return data
+        self._return = _loadData()
+    def join(self):
+        threading.Thread.join(self)
+        return self._return
+
+# with multithreading
+# use with for tqdm to properly shut tqdm down if exception appears
+with tqdm(range(epochs)) as epochLoop:
+  for _ in epochLoop:
+    # loadData
+    loadThread = LoadData(None)
+    loadThread.start()
+    
+    # trainModel
+    trainThread = TrainModel(None)
+    trainThread.start()
+    
+    # only continue if both threads are done
+    modelLoss = trainThread.join()
+    data = loadThread.join()
 
 class ModelAPI:
     def __init__(self):
@@ -42,6 +99,29 @@ class ModelAPI:
         #model = load_model(self.name)
         return "Data loaded"
 
+    def predict(self):
+        x0 = 1
+        x_transf = scale_X.transform([[x0]])
+        print (scale_y.inverse_transform(model.predict(x_transf)))
+
+    def check_overfit(self):
+        eval_train = model.evaluate(X_train, y_train)
+        print('Training: {}'.format(eval_train))
+        eval_test = model.evaluate(X_test, y_test)
+        print('Test: {}'.format(eval_test))
+
+    def create_model(self):
+        x = X_train[:, 0]
+        y = X_train[:, 1]
+        model = Sequential()
+        model.add(Dense(15, input_dim=1, activation='sigmoid'))
+        model.add(Dense(5, activation='sigmoid'))
+        model.add(Dense(1, activation='sigmoid'))
+
+    def fit(self):
+        model.compile(loss='mse', optimizer='adam')
+        model.fit(x, y, epochs=5000, verbose=1)
+        
 m = ModelAPI()
 
 @app.route("/", methods=['GET'])
